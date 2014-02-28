b2bce09bb402c2de17e0bff704c14fc318533a55
Mathilde Ffrench <ffrench.mathilde@gmail.com>
2014-02-28 18:38:47 +0100

    [TAC-85] 0.2 refactoring to use portal war

---
+++ wat/src/main/java/com/spectral/cc/core/injector/wat/consumer/InjectorPluginFacesMBeanRegistryConsumer.java
@@ -0,0 +1,72 @@
+/**
+ * Injector Commons JSF bundle
+ * Injector plugin faces managed bean registry consumer singleton
+ * Copyright (C) 2014 Mathilde Ffrench
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.spectral.cc.core.injector.wat.consumer;
+
+import com.spectral.cc.core.portal.base.registry.FacesMBeanRegistry;
+import org.apache.felix.ipojo.annotations.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * iPojo singleton which consume the plugin faces mbean registry implemented by PortalPluginFacesMBearRegistryImpl.<br/>
+ * Instantiated during directory commons-jsf bundle startup. FactoryMethod : getInstance
+ */
+@Component(publicFactory = false, factoryMethod = "getInstance")
+@Instantiate
+public class InjectorPluginFacesMBeanRegistryConsumer {
+    private static final Logger log = LoggerFactory.getLogger(InjectorPluginFacesMBeanRegistryConsumer.class);
+    private static InjectorPluginFacesMBeanRegistryConsumer INSTANCE;
+
+    @Requires(from="CCPortalFacesMBeanRegistry")
+    private FacesMBeanRegistry portalFacesMBeanRegistry = null;
+
+    @Bind
+    public void bindPortalFacesMBeanRegistry(FacesMBeanRegistry r) {
+        log.debug("Bound to cc portal faces managed bean registry...");
+        portalFacesMBeanRegistry = r;
+    }
+
+    @Unbind
+    public void unbindPortalFacesMBeanRegistry() {
+        log.debug("Unbound from cc portal faces managed bean registry...");
+        portalFacesMBeanRegistry = null;
+    }
+
+    /**
+     * Get portal plugin faces managed bean registry
+     *
+     * @return the binded portal plugin faces managed bean registry. null if unbinded.
+     */
+    public FacesMBeanRegistry getPortalPluginFacesMBeanRegistry() {
+        return portalFacesMBeanRegistry;
+    }
+
+    /**
+     * Factory method for this singleton...
+     *
+     * @return instantiated directory plugin faces mbean registry consumer
+     */
+    public static InjectorPluginFacesMBeanRegistryConsumer getInstance() {
+        if (INSTANCE == null) {
+            INSTANCE = new InjectorPluginFacesMBeanRegistryConsumer();
+        }
+        return INSTANCE;
+    }
+}
---
+++ wat/src/main/java/com/spectral/cc/core/injector/wat/controller/InjectorBreadCrumController.java
@@ -0,0 +1,98 @@
+/**
+ * Injector Commons JSF bundle
+ * Injectors BreadCrum Controller
+ * Copyright (C) 2013 Mathilde Ffrench
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.spectral.cc.core.injector.wat.controller;
+
+import com.spectral.cc.core.injector.wat.consumer.InjectorPluginFacesMBeanRegistryConsumer;
+import com.spectral.cc.core.injector.wat.consumer.InjectorTreeMenuRootsRegistryServiceConsumer;
+import com.spectral.cc.core.portal.base.model.TreeMenuEntity;
+import org.primefaces.component.menuitem.MenuItem;
+import org.primefaces.model.DefaultMenuModel;
+import org.primefaces.model.MenuModel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.faces.context.FacesContext;
+import java.util.ArrayList;
+
+/**
+ * Injector bread crum controller transform injector roots registry into primefaces menu model to be used in injector layout bread crum component.<br/>
+ * This is a request managed bean
+ */
+public class InjectorBreadCrumController {
+    private static final Logger log = LoggerFactory.getLogger(InjectorBreadCrumController.class);
+    private static String MAIN_MENU_INJECTOR_CONTEXT = InjectorPluginFacesMBeanRegistryConsumer.getInstance().getPortalPluginFacesMBeanRegistry().getRegisteredServletContext().getContextPath();
+
+    private MenuModel model     = new DefaultMenuModel();
+
+    private MenuItem createMenuItemFromEntity(TreeMenuEntity entity) {
+        FacesContext context = FacesContext.getCurrentInstance();
+        MenuItem item = new MenuItem();
+        item.setId(entity.getId());
+        if (entity.getContextAddress()!=null && entity.getContextAddress()!="")
+            item.setUrl(context.getExternalContext().getRequestScheme() + "://" +
+                                context.getExternalContext().getRequestServerName() + ":" +
+                                context.getExternalContext().getRequestServerPort() +
+                                entity.getContextAddress());
+        else
+            item.setUrl(context.getExternalContext().getRequestScheme() + "://" +
+                                context.getExternalContext().getRequestServerName() + ":" +
+                                context.getExternalContext().getRequestServerPort() +
+                                MAIN_MENU_INJECTOR_CONTEXT + "/views/main.jsf");
+        item.setValue(entity.getValue());
+        item.setStyleClass("menuItem");
+
+        return item;
+    }
+
+    private MenuItem createRootMenuItem() {
+        FacesContext context = FacesContext.getCurrentInstance();
+        MenuItem item = new MenuItem();
+        item.setId("mainDir");
+        item.setUrl(context.getExternalContext().getRequestScheme() + "://" +
+                            context.getExternalContext().getRequestServerName() + ":" +
+                            context.getExternalContext().getRequestServerPort() +
+                            MAIN_MENU_INJECTOR_CONTEXT + "/views/main.jsf");
+        item.setValue("Injector");
+        item.setStyleClass("menuItem");
+        return item;
+    }
+
+    public MenuModel getModel() {
+        FacesContext context = FacesContext.getCurrentInstance();
+        String contextAddress = MAIN_MENU_INJECTOR_CONTEXT + context.getExternalContext().getRequestServletPath();
+        ArrayList<TreeMenuEntity> orderedBreadScrumMenuFromRootToLeaf = new ArrayList<TreeMenuEntity>();
+        if (InjectorTreeMenuRootsRegistryServiceConsumer.getInstance()!=null) {
+            TreeMenuEntity leaf   = InjectorTreeMenuRootsRegistryServiceConsumer.getInstance().getTreeMenuRootsRegistry().getTreeMenuEntityFromContextAddress(contextAddress);
+            if (leaf!=null) {
+                orderedBreadScrumMenuFromRootToLeaf.add(0,leaf);
+                TreeMenuEntity parent = leaf.getParentTreeMenuEntity();
+                while (parent!=null) {
+                    orderedBreadScrumMenuFromRootToLeaf.add(0,parent);
+                    parent = parent.getParentTreeMenuEntity();
+                }
+
+                model.addMenuItem(createRootMenuItem());
+                for (TreeMenuEntity dir: orderedBreadScrumMenuFromRootToLeaf)
+                    model.addMenuItem(createMenuItemFromEntity(dir));
+            }
+        }
+        return model;
+    }
+
+}
